92488f2c9508add35e700b09baa6ce4b26645883
Stephen Rigdon <stephen@nocapshows.com>
2024-06-23 18:46:27 -0700

    Simplified solution, only addressing the variables in the loadAndBufferSegment method. Memory leak is fixed.

--- memory-leak/memory-leak.js
+++ memory-leak/memory-leak.js
@@ -1,164 +1,157 @@
+(function () {
 
+    // The media playlist URL.
+    // See the HLS (HTTP Live Streaming) specification for more info:
+    //   https://tools.ietf.org/html/rfc8216#section-4.1
+    var MEDIA_PLAYLIST_URL = 'https://lw.bamgrid.com/2.0/hls/vod/bam/ms02/hls/dplus/bao/avc/unenc/8500k/vod.m3u8';
 
-// The media playlist URL.
-// See the HLS (HTTP Live Streaming) specification for more info:
-//   https://tools.ietf.org/html/rfc8216#section-4.1
-var MEDIA_PLAYLIST_URL = 'https://lw.bamgrid.com/2.0/hls/vod/bam/ms02/hls/dplus/bao/avc/unenc/8500k/vod.m3u8';
+    // Keep a mock buffer of 5 segments available for playback.
+    var buffer = [];
+    var bufferedSegments = [];
 
-// Keep a mock buffer of 5 segments available for playback.
-var buffer = [];
-var bufferedSegments = [];
+    // Append MP4 segments to the mock buffer, but keep the buffer size in check to avoid memory overflow.
+    // For some reason, even though we keep the buffer size in check, memory consumption continues to grow.
+    function appendBuffer(segment, arrayBuffer) {
+        buffer.push(arrayBuffer);
+        // buffer.push(segment.arrayBuffer);
+        bufferedSegments.push(segment);
 
-// Keep track of latest timeout id to clear
-var timeout = null;
+        // Remove segments which we no longer need to simulate playback.
+        while (buffer.length > 5) {
+            buffer.shift();
+            bufferedSegments.shift();
+        }
 
-// Append MP4 segments to the mock buffer, but keep the buffer size in check to avoid memory overflow.
-// For some reason, even though we keep the buffer size in check, memory consumption continues to grow.
-function appendBuffer(segment, arrayBuffer) {
-    buffer.push(arrayBuffer);
-    bufferedSegments.push(segment);
+        var totalBufferDuration = bufferedSegments.reduce(function (duration, segment) {
+            return duration + segment.duration;
+        }, 0);
 
-    // Remove segments which we no longer need to simulate playback.
-    while (buffer.length > 5) {
-        buffer[0] = null;
-        buffer.shift();
-        bufferedSegments[0] = null;
-        bufferedSegments.shift();
+        var totalBufferSize = buffer.reduce(function (size, arrayBuffer) {
+            return size + arrayBuffer.byteLength;
+        }, 0);
+
+        var memoryUsed = window.performance.memory.usedJSHeapSize;
+
+        console.log(
+            'Buffered segment: Segments buffered:', buffer.length,
+            'Buffer duration (seconds):', Math.round(totalBufferDuration),
+            'Buffer size (Mb)', Math.round(totalBufferSize / 1000000),
+            'Memory used (Mb)', Math.round(memoryUsed / 1000000)
+        );
     }
 
-    var totalBufferDuration = bufferedSegments.reduce(function (duration, segment) {
-        return duration + segment.duration;
-    }, 0);
+    // Load the media playlist from the CDN. The callback will be called with a String representing
+    // the XHR responseText.
+    function loadPlaylist(url, callback) {
 
-    var totalBufferSize = buffer.reduce(function (size, arrayBuffer) {
-        return size + arrayBuffer.byteLength;
-    }, 0);
+        var xhr = new XMLHttpRequest();
 
-    var memoryUsed = window.performance.memory.usedJSHeapSize;
+        xhr.onerror = function () {
+            console.error('XMLHttpRequest error event occured');
+        };
 
-    console.log(
-        'Buffered segment: Segments buffered:', buffer.length,
-        'Buffer duration (seconds):', Math.round(totalBufferDuration),
-        'Buffer size (Mb)', Math.round(totalBufferSize / 1000000),
-        'Memory used (Mb)', Math.round(memoryUsed / 1000000)
-    );
-}
+        xhr.onload = function () {
+            callback(xhr.responseText);
+        };
 
-// Load the media playlist from the CDN. The callback will be called with a String representing
-// the XHR responseText.
-function loadPlaylist(url, callback) {
+        xhr.open('GET', url);
 
-    var xhr = new XMLHttpRequest();
+        xhr.send(null);
+    }
 
-    xhr.onerror = function () {
-        console.error('XMLHttpRequest error event occured');
-    };
+    // Load an MP4 segment from the CDN. The callback will be called with an ArrayBuffer representing
+    // the MP4 video content data.
+    function loadSegment(url, callback) {
 
-    xhr.onload = function () {
-        callback(xhr.responseText);
-    };
+        var xhr = new XMLHttpRequest();
 
-    xhr.open('GET', url);
+        xhr.onerror = function () {
+            console.error('XMLHttpRequest error event occured');
+        };
 
-    xhr.send(null);
-}
+        xhr.onload = function () {
+            callback(xhr.response);
+        };
 
-// Load an MP4 segment from the CDN. The callback will be called with an ArrayBuffer representing
-// the MP4 video content data.
-function loadSegment(url, callback) {
+        xhr.open('GET', url);
 
-    var xhr = new XMLHttpRequest();
+        xhr.responseType = 'arraybuffer';
 
-    xhr.onerror = function () {
-        console.error('XMLHttpRequest error event occured');
-    };
+        xhr.send(null);
+    }
 
-    xhr.onload = function () {
-        callback(xhr.response);
-    };
+    // A quick (and fragile) way to resolve a segment URL with the playlist URL it belongs to.
+    // This method of URL resolution is not likely to work for other streams.
+    function resolveSegmentURL(base, segmentPath) {
+        return base.replace('vod.m3u8', segmentPath);
+    }
 
-    xhr.open('GET', url);
+    // Load an HLS (HTTP Live Streaming) media playlist and return the parsed segment definitions.
+    function loadAndParsePlaylist(url, callback) {
+        loadPlaylist(url, function (playlist) {
 
-    xhr.responseType = 'arraybuffer';
+            // Keep a list of video MP4 fragments (segments).
+            var fragmentedMP4Segments = [];
 
-    xhr.send(null);
-}
+            var lines = playlist.split('\n');
+            var line;
+            var duration;
+            var currentSegment;
+            var i;
 
-// A quick (and fragile) way to resolve a segment URL with the playlist URL it belongs to.
-// This method of URL resolution is not likely to work for other streams.
-function resolveSegmentURL(base, segmentPath) {
-    return base.replace('vod.m3u8', segmentPath);
-}
+            for (i = 0; i < lines.length; i++) {
+                line = lines[i];
 
-// Load an HLS (HTTP Live Streaming) media playlist and return the parsed segment definitions.
-function loadAndParsePlaylist(url, callback) {
-    loadPlaylist(url, function (playlist) {
+                if (currentSegment) {
+                    currentSegment.url = resolveSegmentURL(url, line);
+                    fragmentedMP4Segments.push(currentSegment);
+                    currentSegment = null;
+                }
 
-        // Keep a list of video MP4 fragments (segments).
-        var fragmentedMP4Segments = [];
+                if (line.indexOf('#EXTINF') === 0) {
+                    duration = line.split(':').pop().replace(/,$/, '');
 
-        var lines = playlist.split('\n');
-        var line;
-        var duration;
-        var currentSegment;
-        var i;
-
-        for (i = 0; i < lines.length; i++) {
-            line = lines[i];
-
-            if (currentSegment) {
-                currentSegment.url = resolveSegmentURL(url, line);
-                fragmentedMP4Segments.push(currentSegment);
-                currentSegment = null;
+                    currentSegment = {
+                        duration: parseFloat(duration)
+                    };
+                }
             }
 
-            if (line.indexOf('#EXTINF') === 0) {
-                duration = line.split(':').pop().replace(/,$/, '');
+            callback(fragmentedMP4Segments);
+        })
+    }
 
-                currentSegment = {
-                    duration: parseFloat(duration)
-                };
+    // Stream the movie into a mock buffer for testing purposes.
+    function streamMovie(playlistUrl) {
+        loadAndParsePlaylist(playlistUrl, function (segmentList) {
+
+            var currentSegmentIndex = 0;
+
+            function loadAndBufferSegment() {
+
+                if (!segmentList[currentSegmentIndex]){
+                    console.log('Done. No more MP4 segments to play');
+                    return;
+                }
+
+                loadSegment(segmentList[currentSegmentIndex].url, function (arrayBuffer) {
+
+                    appendBuffer(segmentList[currentSegmentIndex], arrayBuffer);
+
+                    currentSegmentIndex++
+
+                    // Load a new segment every 2 seconds for this test.
+                    setTimeout(loadAndBufferSegment, 2000);
+                });
             }
-        }
 
-        callback(fragmentedMP4Segments);
-    })
-}
+            loadAndBufferSegment();
+        });
+    }
 
-// Stream the movie into a mock buffer for testing purposes.
-function streamMovie(playlistUrl) {
-    loadAndParsePlaylist(playlistUrl, function (segmentList) {
-
-        var currentSegmentIndex = 0;
-
-        function loadAndBufferSegment() {
-            if (timeout){
-                clearTimeout(timeout);
-            }
-
-            if (!segmentList[currentSegmentIndex]) {
-                console.log('Done. No more MP4 segments to play');
-                return;
-            }
-
-            loadSegment(segmentList[currentSegmentIndex].url, function (arrayBuffer) {
-
-                appendBuffer(segmentList[currentSegmentIndex], arrayBuffer);
-
-                currentSegmentIndex++
-
-                // Load a new segment every 2 seconds for this test.
-                timeout = setTimeout(loadAndBufferSegment, 2000);
-            });
-        }
-
-        loadAndBufferSegment();
+    document.getElementById('run-test-button').addEventListener('click', function () {
+        console.log('Starting test');
+        streamMovie(MEDIA_PLAYLIST_URL);
     });
-}
 
-document.getElementById('run-test-button').addEventListener('click', function () {
-    console.log('Starting test');
-    streamMovie(MEDIA_PLAYLIST_URL);
-});
-
-
+}());
